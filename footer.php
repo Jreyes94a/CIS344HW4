b3f0ffa46d6ab2811c25e4995a311a2a333f97d8
Jreyes94a <jreyes94@outlook.com>
2025-04-30 22:43:31 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- page footer -->
+<footer>
+  <p>&copy; FlyBois Corporation<br />
+    Please see our
+    <a href="legal.php">legal information page</a>.
+  </p>
+
+</footer>
+
+</body>
+
+
+
+</html>
---
+++ home.php
@@ -0,0 +1,12 @@
+<?php
+require('header.php');
+?>
+<!-- page content -->
+<section>
+  <h2>Welcome Aviator to Flybois.</h2>
+  <p>Home of all things flying.</p>
+  <p>Airplanes, helicopters and other flying contraptions welcome.</p>
+</section>
+<?php
+require('footer.php');
+?>
